e2c47e880defd4835a4ae78b3948074d7183e862
James M <jamesmce1215@gmail.com>
2020-02-25 15:07:08 -0700

    Food Wheel page added

---
+++ food_wheel_script.js
@@ -0,0 +1,32 @@
+var wheel = new Image();
+var ctx;
+
+function init() {
+	wheel.src = 'https://i.stack.imgur.com/I7oFb.png';
+	ctx = document.getElementById('canvas').getContext('2d');
+	window.requestAnimationFrame(draw);
+}
+
+function drawImageCenter(image, x, y, cx, cy, scale, rotation){
+    ctx.setTransform(scale, 0, 0, scale, x, y); // sets scale and origin
+    ctx.rotate(rotation);
+    ctx.drawImage(image, -cx, -cy);
+} 
+
+function draw(){
+	var rotate = 0;
+	ctx.clearRect(0, 0, 2000, 2000);
+	ctx.fillRect(0, 0, 2000, 2000);// clear canvas
+	
+	ctx.save();
+	//ctx.translate(100, 100);
+	var time = new Date();
+	rotate = (((2 * Math.PI)) * time.getSeconds() + ((2 * Math.PI) / 1000) * time.getMilliseconds());
+	//ctx.drawImage(sun, 1, 0, 200, 200, 0, 0, 200, 200);
+	drawImageCenter(sun, 150, 150, 100, 100, 1, rotate);
+	ctx.restore();
+
+  window.requestAnimationFrame(draw);
+}
+
+init();
