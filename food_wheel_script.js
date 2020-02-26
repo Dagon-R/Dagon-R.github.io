3089e9800482dd2d30376852b2c070a77dc9ef00
Dagon-R <49168604+Dagon-R@users.noreply.github.com>
2020-02-25 17:09:37 -0700

    Update food_wheel_script.js

--- food_wheel_script.js
+++ food_wheel_script.js
@@ -1,4 +1,5 @@
 var wheel = new Image();
+var arrow = new Image();
 var ctx;
 var power = 0;
 var rotate = 0;
@@ -14,15 +15,20 @@ const red = 315;
 
 function init() {
 	wheel.src = 'https://upload.wikimedia.org/wikipedia/commons/d/dc/Eight-colour-wheel-2D.png';
+	arrow.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Arrow_east.svg/640px-Arrow_east.svg.png';
 	ctx = document.getElementById('canvas').getContext('2d');
 	ctx.canvas.width  = window.innerWidth;
   	ctx.canvas.height = (window.innerHeight) * .8;
+	var time = new Date();
+	rotate = (2 * Math.PI) * (time.getMilliseconds() / 1000);
+	drawImageCenter(wheel, 50, 50, 550, 550, 1, rotate);
+	drawImageCenter(arrow, 600, 600, 320, 230, 1, (.75 * Math.PI));
 }
 
 function spin(){
 	var time = new Date();
 	power = .2;
-	rotate = (2 * Math.PI) * (time.getMilliseconds() / 1000)
+	rotate = (2 * Math.PI) * (time.getMilliseconds() / 1000);
 	window.requestAnimationFrame(draw);
 }
 
@@ -34,13 +40,13 @@ function drawImageCenter(image, x, y, cx, cy, scale, rotation){
 
 function draw(){
 	ctx.clearRect(0, 0, 2000, 2000);
-	
+	drawImageCenter(arrow, 600, 600, 320, 230, 1, (.75 * Math.PI));
 	ctx.save();
 	//ctx.translate(100, 100);
 	
 	rotate = (rotate + power) % (2 * Math.PI);
 	//ctx.drawImage(sun, 1, 0, 200, 200, 0, 0, 200, 200);
-	drawImageCenter(wheel, 0, 0, 550, 550, 1, rotate);
+	drawImageCenter(wheel, 50, 50, 550, 550, 1, rotate);
 	ctx.restore();
 	if(power > 0){
 		power -= .001;
