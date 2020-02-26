0f6357e99890542e5259a2f73f04a322f273139f
Dagon-R <49168604+Dagon-R@users.noreply.github.com>
2020-02-25 17:14:23 -0700

    Update food_wheel_script.js

--- food_wheel_script.js
+++ food_wheel_script.js
@@ -21,8 +21,12 @@ function init() {
   	ctx.canvas.height = (window.innerHeight) * .8;
 	var time = new Date();
 	rotate = (2 * Math.PI) * (time.getMilliseconds() / 1000);
+	window.requestAnimationFrame(initDraw);
+}
+
+function initDraw(){
 	drawImageCenter(wheel, 50, 50, 550, 550, 1, rotate);
-	drawImageCenter(arrow, 600, 600, 320, 230, 1, (.75 * Math.PI));
+	drawImageCenter(arrow, 600, 600, 320, 230, 1, (1.25 * Math.PI));
 }
 
 function spin(){
@@ -40,20 +44,20 @@ function drawImageCenter(image, x, y, cx, cy, scale, rotation){
 
 function draw(){
 	ctx.clearRect(0, 0, 2000, 2000);
-	drawImageCenter(arrow, 600, 600, 320, 230, 1, (1.25 * Math.PI));
+	
 	ctx.save();
 	//ctx.translate(100, 100);
-	
 	rotate = (rotate + power) % (2 * Math.PI);
 	//ctx.drawImage(sun, 1, 0, 200, 200, 0, 0, 200, 200);
 	drawImageCenter(wheel, 50, 50, 550, 550, 1, rotate);
 	ctx.restore();
+	drawImageCenter(arrow, 600, 600, 320, 230, 1, (1.25 * Math.PI));
 	if(power > 0){
 		power -= .001;
 		window.requestAnimationFrame(draw);
 	}
 	else{
-	 power = 0;	
+		power = 0;	
 	}
 }
 
