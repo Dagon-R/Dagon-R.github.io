a6ca492fbc519a8e604842b068bde7d28e2d4e6a
James M <jamesmce1215@gmail.com>
2020-03-21 22:43:54 -0600

    testing intital draw fix 3

--- food_wheel_script.js
+++ food_wheel_script.js
@@ -15,16 +15,28 @@ const violet = 270;
 const red = 315;
 
 function init() {
-	wheel.src = 'https://upload.wikimedia.org/wikipedia/commons/d/dc/Eight-colour-wheel-2D.png';
-	arrow.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Arrow_east.svg/640px-Arrow_east.svg.png';
+	
 	ctx = document.getElementById('canvas').getContext('2d');
 	ctx.canvas.width  = window.innerWidth;
   	ctx.canvas.height = (window.innerHeight) * .8;
 	var time = new Date();
 	rotate = (2 * Math.PI) * (time.getMilliseconds() / 1000);
+	//window.requestAnimationFrame(initDraw);
+	setSrc(window.requestAnimationFrame(initDraw));
 }
 
+function initDraw(){
+	var rotate = (2 * Math.PI) * (time.getMilliseconds() / 1000);
+	ctx.clearRect(0, 0, 2000, 2000);
+	drawImageCenter(wheel, 50, 50, 550, 550, 1, rotate);
+	drawImageCenter(arrow, 600, 600, 320, 230, 1, (1.25 * Math.PI));
+}
 
+function setSrc(callback) {
+	wheel.onload = callback;
+	wheel.src = 'https://upload.wikimedia.org/wikipedia/commons/d/dc/Eight-colour-wheel-2D.png';
+	arrow.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Arrow_east.svg/640px-Arrow_east.svg.png';
+}
 
 function spin(){
 	if(generated){
