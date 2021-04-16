8fbdf87295cfa54474f50cb0f66b829879ad0438
James M <jamesmce1215@gmail.com>
2021-04-15 23:09:56 -0700

    Got the react working! We now have a nice self-formatting website!

--- resume_page_react.js
+++ resume_page_react.js
@@ -1,18 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
 var parser = new DOMParser();
 var xhttp = new XMLHttpRequest();
 
-function parseFile() {
-	if (this.readyState == 4 && this.status == 200) {
-		// Typical action to be performed when the document is ready:
-		var xmlDoc = parse.parseFromString(text, xhttp.responseText);
-		renderXML(xmlDoc);
-	}
-}
-
-function renderXML() {
+function renderXML(xmlDoc) {
 	var sectionList = xmlDoc.getElementsByTagName("section");
 	for (var i = 0; i < sectionList.length; i++) {
 		renderSection(sectionList[i]);
@@ -23,45 +13,121 @@ function renderSection(section) {
 	var entryList = section.getElementsByTagName("entry");
 	var sectionName = section.getAttribute("title");
 	var root = document.getElementById(sectionName);
+	var entryArray = [];
 	for (var i = 0; i < entryList.length; i++) {
 		var entry = entryList[i];
+		var title = null;
+		var subtitle = null;
+		var description = null;
+		var technology = null;
+		var url = null;
+		if (entry.getElementsByTagName("title")[0].hasChildNodes()) {
+			title = entry.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+		}
+		if (entry.getElementsByTagName("subtitle")[0].hasChildNodes()) {
+			subtitle = entry.getElementsByTagName("subtitle")[0].childNodes[0].nodeValue;
+		}
+		if (entry.getElementsByTagName("description")[0].hasChildNodes()) {
+			description = entry.getElementsByTagName("description")[0].childNodes[0].nodeValue;
+		}
+		if (entry.getElementsByTagName("technology")[0].hasChildNodes()) {
+			technology = entry.getElementsByTagName("technology")[0].childNodes[0].nodeValue;
+		}
+		if (entry.getElementsByTagName("url")[0].hasChildNodes()) {
+			url = entry.getElementsByTagName("url")[0].childNodes[0].nodeValue;
+		}
 		var entryObject = {
-			title: entry.getElementsByTagName(title)[0].childNodes[0].nodeValue,
-			subtitle: entry.getElementsByTagName(title)[0].childNodes[0].nodeValue,
-			description: entry.getElementsByTagName(title)[0].childNodes[0].nodeValue,
-			technology: entry.getElementsByTagName(title)[0].childNodes[0].nodeValue
+			title: title || "",
+			subtitle: subtitle || "",
+			description: description || "",
+			technology: technology || "",
+			url: url || ""
 		};
-		var newEntry = React.createElement(BasicEntry, { entry: EntryObject });
-		ReactDOM.render(newEntry, root);
+		var newEntry = React.createElement(LeftEntry, { entry: entryObject });
+		entryArray.push(newEntry);
+		if (i != entryList.length - 1) {
+			entryArray.push(React.createElement(Divider, null));
+		}
 	}
+	ReactDOM.render(entryArray, root);
 }
 
-function BasicEntry(props) {
+function LeftEntry(props) {
+	var link;
+	if (props.entry.url == "") {
+		link = null;
+	} else {
+		link = props.entry.url;
+	}
 	return React.createElement(
-		'div',
-		{ className: 'container' },
+		"div",
+		{ className: "container" },
 		React.createElement(
-			'div',
-			{ className: "basic-entry" },
+			"div",
+			{ className: "left-entry" },
 			React.createElement(
-				'div',
-				{ className: 'entry-header' },
-				props.entry.title
+				"div",
+				{ className: "left-entry-left-column" },
+				React.createElement(
+					"div",
+					{ className: "entry-header" },
+					React.createElement(
+						"h5",
+						null,
+						React.createElement(
+							"b",
+							null,
+							props.entry.title
+						)
+					)
+				),
+				React.createElement(
+					"div",
+					{ className: "entry-subtitle" },
+					React.createElement(
+						"h5",
+						null,
+						props.entry.subtitle
+					)
+				),
+				React.createElement(
+					"div",
+					{ className: "entry-technology" },
+					props.entry.technology
+				),
+				React.createElement(
+					"div",
+					{ className: "entry-link" },
+					props.entry.url != "" && React.createElement(
+						"a",
+						{ className: "button", href: link, target: "_blank", rel: "noopener noreferrer" },
+						"Link"
+					)
+				)
 			),
 			React.createElement(
-				'div',
-				{ className: 'entry-subtitle' },
-				props.entry.subtitle
-			),
-			React.createElement(
-				'div',
-				{ className: 'entry-description' },
-				props.entry.description
+				"div",
+				{ className: "left-entry-right-column" },
+				React.createElement(
+					"div",
+					{ className: "entry-description" },
+					props.entry.description
+				)
 			)
 		)
 	);
 }
 
-xhttp.onreadystatechange = parseFile();
-xhttp.open("GET", "resume_page_react.xml", true);
+function Divider() {
+	return React.createElement("hr", { className: "entry-divider" });
+}
+
+xhttp.onreadystatechange = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		// Typical action to be performed when the document is ready:
+		var xmlDoc = xhttp.responseXML;
+		renderXML(xmlDoc);
+	}
+};
+xhttp.open("GET", "resume_page_xml.xml", true);
 xhttp.send();
--- src/resume_page_react.js
+++ src/resume_page_react.js
@@ -1,18 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
 var parser= new DOMParser();
 var xhttp = new XMLHttpRequest();
 
-function parseFile(){
-	if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       var xmlDoc = parse.parseFromString(text, xhttp.responseText);
-	   renderXML(xmlDoc);
-    }
-}
-
-function renderXML(){
+function renderXML(xmlDoc){
 	var sectionList = xmlDoc.getElementsByTagName("section");
 	for(var i = 0; i < sectionList.length; i++){
 		renderSection(sectionList[i]);
@@ -23,37 +13,90 @@ function renderSection(section){
 	var entryList = section.getElementsByTagName("entry");
 	var sectionName = section.getAttribute("title");
 	var root = document.getElementById(sectionName);
+	var entryArray = [];
 	for(var i = 0; i < entryList.length; i++){
 		var entry = entryList[i];
+		var title = null;
+		var subtitle = null;
+		var description = null;
+		var technology = null;
+		var url = null;
+		if(entry.getElementsByTagName("title")[0].hasChildNodes()){
+			title = entry.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+		}
+		if(entry.getElementsByTagName("subtitle")[0].hasChildNodes()){
+			subtitle = entry.getElementsByTagName("subtitle")[0].childNodes[0].nodeValue;
+		}
+		if(entry.getElementsByTagName("description")[0].hasChildNodes()){
+			description = entry.getElementsByTagName("description")[0].childNodes[0].nodeValue;
+		}
+		if(entry.getElementsByTagName("technology")[0].hasChildNodes()){
+			technology = entry.getElementsByTagName("technology")[0].childNodes[0].nodeValue;
+		}
+		if(entry.getElementsByTagName("url")[0].hasChildNodes()){
+			url = entry.getElementsByTagName("url")[0].childNodes[0].nodeValue;
+		}
 		var entryObject = {
-			title: entry.getElementsByTagName(title)[0].childNodes[0].nodeValue,
-			subtitle: entry.getElementsByTagName(title)[0].childNodes[0].nodeValue,
-			description: entry.getElementsByTagName(title)[0].childNodes[0].nodeValue,
-			technology: entry.getElementsByTagName(title)[0].childNodes[0].nodeValue
+			title: title || "",
+			subtitle: subtitle || "",
+			description: description || "",
+			technology: technology || "",
+			url: url || ""
 		};
-		const newEntry = <BasicEntry entry={EntryObject} />;
-		ReactDOM.render(newEntry,root);
+		const newEntry = <LeftEntry entry={entryObject} />;
+		entryArray.push(newEntry);
+		if(i != entryList.length - 1){
+			entryArray.push(<Divider />);
+		}
 	}
+	ReactDOM.render(entryArray,root);
 }
 
-function BasicEntry(props){
+function LeftEntry(props){
+	var link;
+	if(props.entry.url == ""){
+		link = null;
+	}
+	else{
+		link = props.entry.url;
+	}
 	return (
 		<div className="container">
-			<div className={"basic-entry"}>
-				<div className="entry-header">
-					{props.entry.title}
+			<div className="left-entry">
+				<div className="left-entry-left-column">
+					<div className="entry-header">
+						<h5><b>{props.entry.title}</b></h5>
+					</div>
+					<div className="entry-subtitle">
+						<h5>{props.entry.subtitle}</h5>
+					</div>
+					<div className="entry-technology">
+						{props.entry.technology}
+					</div>
+					<div className="entry-link">
+						{props.entry.url != "" && <a className="button" href={link} target="_blank" rel="noopener noreferrer">Link</a>}
+					</div>
 				</div>
-				<div className="entry-subtitle">
-					{props.entry.subtitle}		
-				</div>
-				<div className="entry-description">
-					{props.entry.description}		
+				<div className="left-entry-right-column">
+					<div className="entry-description">
+						{props.entry.description}		
+					</div>
 				</div>
 			</div>
 		</div>
 	);
 }
 
-xhttp.onreadystatechange = parseFile();
-xhttp.open("GET", "resume_page_react.xml", true);
+function Divider(){
+	return <hr className="entry-divider"/>;
+}
+
+xhttp.onreadystatechange = function(){
+	if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       var xmlDoc = xhttp.responseXML;
+	   renderXML(xmlDoc);
+    }
+};
+xhttp.open("GET", "resume_page_xml.xml", true);
 xhttp.send();
